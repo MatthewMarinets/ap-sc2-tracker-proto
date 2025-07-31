f249ac11cbf9899a19bf87ce970577a31aa9f03e
Matthew <matthew.marinets@gmail.com>
2025-07-30 23:19:27 -0700

    Adding keys section; stub locations section; hero items minimized at start; blur effect on uncollected items

--- sc2tracker.js
+++ sc2tracker.js
@@ -19,7 +19,36 @@ let setAcquired = (itemname) => {
     }
 }
 
+let getKeyCounts = () => {
+    let keyNodes = document.getElementById("keylist").children;
+    let result = {};
+    for (let i = 0; i < keyNodes.length; ++i) {
+        result[keyNodes[i].id] = parseInt(keyNodes[i].getAttribute("data-count"));
+    }
+    return result;
+}
+
+let addKeyNode = (keyname) => {
+    let newNode = document.createElement("li");
+    newNode.id = keyname;
+    newNode.setAttribute("data-count", 1);
+    newNode.appendChild(document.createTextNode(keyname));
+    document.getElementById("keylist").appendChild(newNode);
+}
+
+let addKey = (keyname) => {
+    let keyCounts = getKeyCounts();
+    if (keyCounts[keyname] != null) {
+        let keyNode = document.getElementById(keyname);
+        keyNode.setAttribute("data-count", keyCounts[keyname] + 1);
+        keyNode.innerText = keyname + " (" + keyNode.getAttribute("data-count") + ")"
+    } else {
+        addKeyNode(keyname);
+    }
+}
+
 onload = () => {
+    // test code
     setAcquired("Ghost");
     setAcquired("Shaped Blast (Siege Tank)");
     setAcquired("Valkyrie");
